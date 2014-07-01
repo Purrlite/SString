d112f98e209f00309e4ce68eea62f6113ee95b7d
Purlox <purlox@gmail.com>
2014-07-01 16:25:34 +0200

    Changed sizes and automated the initialization of num_of_strings.

--- test.c
+++ test.c
@@ -37,8 +37,8 @@ int main(void) {
 	const char * test_strings[] = {
 #include "lorem_ipsum.txt"
 	};
-	const int num_of_strings = 6;
-    const int size_of_strings[6] = { 8, 16, 32, 64, 128, 256 };
+	const int num_of_strings = sizeof(test_strings) / 8;
+    const int size_of_strings[6] = { 8, 64, 1024 };
 
 
 	for(i = 0; i < NUM_OF_LOOPS; i++) {
