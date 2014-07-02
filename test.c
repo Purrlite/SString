f06f05d744abb31b95ee88c20d058b5cc35fdda7
Purlox <purlox@gmail.com>
2014-07-03 01:52:20 +0200

    Increased the amount of loops.
    Mainly done to make the avarages more accurate.

--- test.c
+++ test.c
@@ -10,7 +10,7 @@
 #define START_COUNTING  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start)
 #define END_COUNTING    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end)
 
-#define NUM_OF_LOOPS  1024
+#define NUM_OF_LOOPS  8192
 
 long
 time_difference(struct timespec end,
