33257251bfd8791a99f693f89ad508136240f1e8
Purlox <purlox@gmail.com>
2014-07-26 00:42:00 +0200

    Removed the slower version of copy_n_sstring and left the fast one there.

--- sstring.c
+++ sstring.c
@@ -161,37 +161,6 @@ copy_n_sstring(SString * restrict destination,
                size_t num)
 {
 	size_t length = (start + num > source->length) ? source->length - start : num;
-
-	if(NULL == destination  ||  NULL == source  ||  NULL == source->string
-		    ||  0 == num  ||  start >= source->length)
-		return -1;
-
-	if(length > destination->size  ||  NULL == destination->string) {
-		destination->size = length + 1;
-		if(NULL != destination->string)
-			free(destination->string);
-		destination->string = malloc(destination->size);
-
-		if(NULL == destination->string)
-			return -2;
-	}
-
-	strncpy(destination->string, &(source->string[start]), length);
-	destination->string[length] = '\0';
-
-	destination->length = length;
-
-	return 1;
-}
-
-
-int
-copy_n_sstring2(SString * restrict destination,
-                const SString * restrict source,
-                size_t start,
-                size_t num)
-{
-	size_t length = (start + num > source->length) ? source->length - start : num;
 	char temp;
 
 	if(NULL == destination  ||  NULL == source  ||  NULL == source->string
