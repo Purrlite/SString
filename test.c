a3b738858c523ed9fca60e3bd0564b12aa44626e
Purlox <purlox@gmail.com>
2014-07-26 14:07:13 +0200

    Fixed the tests to use proper arguments.

--- test.c
+++ test.c
@@ -31,7 +31,7 @@ int main(void) {
 		s_str = new_sstring(test_strings[i], size_of_strings[i] * 2 + 1);
 		s_str2 = new_sstring(test_strings[i], size_of_strings[i] + 1);
 
-		append_n_sstring(s_str, s_str2, size_of_strings[i]);
+		append_n_sstring(&s_str, &s_str2, size_of_strings[i]);
 
 		free(s_str.string);
 		free(s_str2.string);
@@ -40,7 +40,7 @@ int main(void) {
 		s_str = new_sstring(test_strings[i], size_of_strings[i] * 2 + 1);
 		s_str2 = new_sstring(test_strings[i], size_of_strings[i] + 1);
 
-		append_n_sstring2(s_str, s_str2, size_of_strings[i]);
+		append_n_sstring2(&s_str, &s_str2, size_of_strings[i]);
 
 		free(s_str.string);
 		free(s_str2.string);
