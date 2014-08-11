8332df5f967401822167641aba5584b55196eea7
Purlox <purlox@gmail.com>
2014-08-11 21:47:57 +0200

    Fixed connect_sstrings() to work as documented.

--- sstring.c
+++ sstring.c
@@ -158,8 +158,7 @@ connect_sstrings(const struct SStrings * strs,
 	size_t i;
 	SString str = (SString){0};
 
-	if(strs == NULL  ||  strs->length == 0  ||  connector == NULL
-		    ||  connector->string == NULL)
+	if(strs == NULL  ||  strs->length == 0)
 		return (SString){0};
 
 	for(i = 0; i < strs->length - 1; i++) {
